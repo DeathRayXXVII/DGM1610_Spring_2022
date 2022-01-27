0d1f9cff8e194e25ebe3c924cbe9e7c9618f543d
Riley Anderson <10803452@uvu.edu>
2022-01-26 17:13:32 -0700

    Worked on some if statements

---
+++ DGM1610_Game2/Assets/Scripts/Script_Assignments/Variables_Conditinals/If_Statments.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class If_Statments : MonoBehaviour
+{
+
+    // public string lightColor;
+
+    // public string weather;
+
+    public string hairColor;
+    public int age;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if(lightColor == "Red")
+        {
+            Debug.Log("The Light is "+ lightColor +" SOTP!!");
+        }
+        else if (lightColor == "Yellow")
+        {
+            Debug.Log("The light is "+ lightColor +" Slow Down the HELL Down!!");
+        }
+        else if (lightColor == "Green")
+        {
+            Debug.Log("The light is "+ lightColor +" You are Free to Go!!");
+        }
+        else
+        {
+            Debug.Log("There isn't a stop light of the color");
+        }*/
+
+        /*if(weather == "Rain" || weather == "Snow")
+        {
+            Debug.Log("The Weather is aabad, you shouyld wear a coat!");
+        }
+        else if (weather == "Sunny"|| weather == "Cloudy")
+        {
+            Debug.Log("You dont need a coat!");
+        }*/
+
+        if (hairColor == "Red" && age == 18)
+        {
+            Debug.Log("You can rid the Fire Dragon ride");
+        }
+        else if (hairColor == "Blonde" && age == 100)
+        {
+            Debug.Log("You are old enough to ride the unicorn ride");
+        }
+    }
+}
