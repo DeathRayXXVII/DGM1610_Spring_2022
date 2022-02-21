634b2202b9e62d153f15c09712edc5918c675d44
Riley Anderson <10803452@uvu.edu>
2022-02-20 22:01:54 -0700

    Finished my Arrays assignment

--- Prototype_2/Assets/Script/Arrays/Arrays.cs
+++ Prototype_2/Assets/Script/Arrays/Arrays.cs
@@ -9,12 +9,18 @@ public class Arrays : MonoBehaviour
     public string[] food = {"Pizza", "Chesseburgar", "Enchaloda", "Chicken Sandwich", "French Toast"};
     public int[] age = {16, 18, 20, 25};
     public string[] drinkFlavors = {"Coke", "Pepsi"};
+    public float[] decimals = {0.2f, 0.5f, 0.10f};
+    public int[] days = {15, 18, 5, 20};
+    //public bool[] consoleWar;
+    public string[] consoleWars = {"Playstation", "Xbox", "Nintendo"};
+    public float[] calc = {0.78f, 0.96f, 0.135f, 0.759f};
+    public int[] number2 = {56134, 894, 48133, 124536};
     
 
     // Start is called before the first frame update
     void Start()
     {
-        System.Random random = new System.Random();
+        //System.Random random = new System.Random();
         Debug.Log("former friend " + freindNames[2]);
         Debug.Log(freindNames[1]+ " says hey!");
         freindNames[0] = "Veronica";
@@ -32,13 +38,26 @@ public class Arrays : MonoBehaviour
         {
             print(age);
         }
+
         Debug.Log(drinkFlavors[0] + " is better than " + drinkFlavors[1]);
         
-    }
+        foreach(float decimals in decimals)
+        {
+            print(decimals);
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        int total;
+        total = numbers[0] * age[1] + days[2 / 3 - 0 + 1];
+        print(total + " Days have passed since your awakening!");
         
+        Debug.Log(freindNames[1] + " Bought you a new console " + consoleWars[0] + " Instead of " + consoleWars[1]);
+        
+        float calcD;
+        calcD = calc[0] + calc[2] * calc[1] / calc[3];
+        Debug.Log("You have " + calcD + " Hours tell you die!!!" );
+        
+        int total2;
+        total2 = numbers[0] + numbers[1] + numbers[2] / numbers[3];
+        Debug.Log( numbers[0] + " + " + number2[1] + " + " + number2[2] + " + " + number2[3] + " = " + total2 + " or your MOM! ohohohoh!!");
     }
 }
