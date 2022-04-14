db4715e9ed7387074390cfa9eb3e986356ea05a8
Riley Anderson <10803452@uvu.edu>
2022-04-14 10:28:11 -0600

    did some for loops and froze unity cool

--- Retro_FPS_Clone/Assets/Scripts/Assignments/Loops.cs
+++ Retro_FPS_Clone/Assets/Scripts/Assignments/Loops.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class Loops : MonoBehaviour
 {
-    //private int numEwoks = 0;
-    private int ducksInPond = 99;
+    private int numEwoks = 0;
+    private int numClones = 20;
+    private int numDeadST = 0;
+    private int numTieFighter = 0;
+    private int numYunglings = 50;
+    private int ducksInPond = 50;
     // Start is called before the first frame update
     void Start()
     {
-        /*for(int i = 20; i > numEwoks; i--)
+        for(int i = 20; i > numEwoks; i--)
         {
             Debug.Log("There are " + i + " Ewaks on a log!");
-        }*/
+        }
+        for(int i = 0; i < numClones; i--)
+        {
+            Debug.Log( i + " more Clone Troopers are on there way!");
+        }
+        for(int i = 50; i > numDeadST; i--)
+        {
+            Debug.Log( "Theses Strom Troopers cant aim " + i);
+        }
+        for(int i = 20; i < numTieFighter; i++)
+        {
+            Debug.Log( "Number of Tie Fighters shot down " + i);
+        }
+        for(int i = 0; i > numYunglings; i--)
+        {
+             Debug.Log( "I have merderd  " + i + " Jedi younlings");
+        }
 
-        while(ducksInPond > 0)
+        /*while(ducksInPond > 0)
         {
             Debug.Log(ducksInPond);
             Debug.Log("All the ducks have flown south for the Winter");
             ducksInPond --;
-        }
+        }*/
 
     }
 
