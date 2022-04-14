d0103acbb964716aa85da776fcb0811992932e86
Riley Anderson <10803452@uvu.edu>
2022-04-14 17:02:01 -0600

    Fixed the for loops and did some while loops

--- Retro_FPS_Clone/Assets/Scripts/Assignments/Loops.cs
+++ Retro_FPS_Clone/Assets/Scripts/Assignments/Loops.cs
@@ -9,15 +9,21 @@ public class Loops : MonoBehaviour
     private int numDeadST = 0;
     private int numTieFighter = 0;
     private int numYunglings = 50;
+
     private int ducksInPond = 50;
+    private int bankAcc = 0;
+    private int daysTellDeath = 30;
+    private int legoBoxes = 0;
+    private int fPS = 45;
     // Start is called before the first frame update
     void Start()
     {
+        
         for(int i = 20; i > numEwoks; i--)
         {
             Debug.Log("There are " + i + " Ewaks on a log!");
         }
-        for(int i = 0; i < numClones; i--)
+        for(int i = 0; i < numClones; i++)
         {
             Debug.Log( i + " more Clone Troopers are on there way!");
         }
@@ -25,27 +31,42 @@ public class Loops : MonoBehaviour
         {
             Debug.Log( "Theses Strom Troopers cant aim " + i);
         }
-        for(int i = 20; i < numTieFighter; i++)
+        for(int i = 20; i > numTieFighter; i--)
         {
             Debug.Log( "Number of Tie Fighters shot down " + i);
         }
-        for(int i = 0; i > numYunglings; i--)
+        for(int i = 0; i < numYunglings; i++)
         {
              Debug.Log( "I have merderd  " + i + " Jedi younlings");
         }
-
-        /*while(ducksInPond > 0)
+        
+        while(ducksInPond > 0)
         {
             Debug.Log(ducksInPond);
             Debug.Log("All the ducks have flown south for the Winter");
             ducksInPond --;
-        }*/
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+        while(bankAcc < 20)
+        {   
+            Debug.Log(bankAcc);
+            Debug.Log("You got paid");
+            bankAcc ++;
+        }
+       while(daysTellDeath > 0)
+        {
+            Debug.Log(daysTellDeath);
+            Debug.Log("Enjoy the time you have left");
+            daysTellDeath --;
+        }
+         while(legoBoxes < 15)
+        {
+            Debug.Log("You have " + legoBoxes + " Lego boxes!");
+            legoBoxes ++;
+        }
+        while(fPS > 0)
+        {
+            Debug.Log("Your FPS is trash! " + fPS);
+            fPS --;
+        }
     }
 }
