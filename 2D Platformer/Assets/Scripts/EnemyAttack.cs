814fe6a722bc0482be4605898e16e87e3e64e81b
Riley Anderson <10803452@uvu.edu>
2022-04-21 19:49:51 -0600

    Added an Enemy attack system

---
+++ 2D Platformer/Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    [Header ("Enemy Attack")]
+    public int damage;
+    public float attackRange; //Range in which the player can attack
+    public float attackRate;
+    private float lastAttackTime;
+    public PlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* if(Time.time - lastAttackTime >= attackRate && Vector2.Distance(transform.position, player.transform.position) < attackRange)
+        {
+            OnTriggerEnter2D();
+        }*/
+    }
+    private void OnTriggerEnter2D (Collider2D other)
+    {
+        lastAttackTime = Time.time;
+        player.TakeDamage(damage);
+    }
+}
