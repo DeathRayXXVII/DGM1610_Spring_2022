5b623f8893d13626c1baccc092013debcffe5670
Riley Anderson <10803452@uvu.edu>
2022-04-23 10:57:55 -0600

    Added a separate script for the camera

---
+++ 2D Platformer/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public PlayerController player; // find the player
+    public bool isFollowing; //making sure the camera is fallowing
+    public float xOffset; //x offset for camera
+    public float yOffset; // y offset for camera
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerController>();
+        isFollowing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFollowing)
+        {
+            transform.position = new Vector3(player.transform.position.x + xOffset, player.transform.position.y + yOffset, transform.position.z);
+        }
+    }
+}
