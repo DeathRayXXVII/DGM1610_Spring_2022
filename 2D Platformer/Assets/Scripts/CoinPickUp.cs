75378749a78bb3fcc9b24277e477aa374656b777
Riley Anderson <10803452@uvu.edu>
2022-04-24 20:33:49 -0600

    Added a Score Manager with a coin pick up

---
+++ 2D Platformer/Assets/Scripts/CoinPickUp.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickUp : MonoBehaviour
+{
+    public int scoreToGive; //Score for coin pick up
+    public ScoreManager scoreManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) 
+    {
+        scoreManager.IncreaseScoreText(scoreToGive);
+        Destroy(gameObject);
+    }
+}
